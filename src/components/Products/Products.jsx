07d5a69267ca36f577de9f003468f2a16324fad1
vag0st <zomgwtfwow@hotmail.com>
2023-02-10 19:49:59 +0200

    ADD Products component and css with rotate animation

---
+++ src/components/Products/Products.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import PropTypes from 'prop-types';
+import './Products.css';
+
+function Product({ name, price, image }) {
+  return (
+    <div className="product">
+      <div className="product-image">
+        <img src={image} alt={name} />
+      </div>
+      <div className="product-info">
+        <h3>{name}</h3>
+        <p>
+          Price: $
+          {price}
+        </p>
+        <p>
+          Recipe: asd
+        </p>
+      </div>
+    </div>
+  );
+}
+
+Product.propTypes = {
+  name: PropTypes.string.isRequired,
+  price: PropTypes.number.isRequired,
+  image: PropTypes.string.isRequired,
+};
+
+function ProductsPage() {
+  return (
+    <div>
+      <h2>Our Products</h2>
+      <div className="products-container">
+        <Product name="Product 1" price={9.99} image="https://via.placeholder.com/150" />
+        <Product name="Product 2" price={19.99} image="https://via.placeholder.com/150" />
+        <Product name="Product 3" price={29.99} image="https://via.placeholder.com/150" />
+        <Product name="Product 4" price={39.99} image="https://via.placeholder.com/150" />
+      </div>
+    </div>
+  );
+}
+
+export default ProductsPage;
