23db62eebbb9e9f884429a8bfb8820df5f1a950b
vag0st <zomgwtfwow@hotmail.com>
2023-02-15 01:49:07 +0200

    ADD Age Disclaimer functionality that uses local storage

--- src/components/AgeDisclaimer/AgeDisclaimer.jsx
+++ src/components/AgeDisclaimer/AgeDisclaimer.jsx
@@ -1,29 +1,31 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './AgeDisclaimer.css';
 
 function AgeDisclaimer() {
   const [showDisclaimer, setShowDisclaimer] = useState(true);
-  const [ageVerified, setAgeVerified] = useState(false);
 
-  console.log(ageVerified);
+  useEffect(() => {
+    const ageVerification = localStorage.getItem('ageVerification');
+    if (ageVerification) {
+      setShowDisclaimer(false);
+    }
+  }, []);
 
   const handleVerification = () => {
-    setAgeVerified(true);
     setShowDisclaimer(false);
+    localStorage.setItem('ageVerification', true);
   };
 
   return (
-    <div>
-      {showDisclaimer && (
-        <div className="age-disclaimer">
-          <div className="age-disclaimer-content">
-            <p>Are you over the age of 18?</p>
-            <button type="submit" onClick={handleVerification}>Yes</button>
-            <button type="submit" onClick={() => setShowDisclaimer(false)}>No</button>
-          </div>
+    showDisclaimer && (
+      <div className="age-disclaimer">
+        <div className="age-disclaimer-content">
+          <p>Are you over the age of 18?</p>
+          <button type="submit" onClick={handleVerification}>Yes</button>
+          <button type="submit" onClick={() => setShowDisclaimer(false)}>No</button>
         </div>
-      )}
-    </div>
+      </div>
+    )
   );
 }
 
