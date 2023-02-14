86f64bcc900fbb1cad0dbdd3fccadfdbe0c5ebb9
vag0st <zomgwtfwow@hotmail.com>
2023-02-15 01:55:37 +0200

    UPDATE redirect to homepage when the logo in Header component is clicked

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Header.css';
 
 function Header() {
   return (
     <div>
       <header className="header">
-        <img src="/assets/images/logo.png" alt="Ouzo Logo" className="header__logo" />
+        <Link to="/">
+          <img src="/assets/images/logo.png" alt="Ouzo Logo" className="header__logo" />
+        </Link>
         <h1 className="header__title">Ouzo Nikolaou</h1>
       </header>
     </div>
