66d501bae2dbaec798184eaf0a0764154b87720c
vag0st <zomgwtfwow@hotmail.com>
2023-02-15 01:40:13 +0200

    ADD CookieBanner functionality

--- src/components/CookieBanner/CookieBanner.jsx
+++ src/components/CookieBanner/CookieBanner.jsx
@@ -6,9 +6,20 @@ function CookieBanner() {
 
   const dismissBanner = () => {
     setDisplay(false);
+    document.cookie = 'cookieConsent=true; max-age=31536000; path=/';
   };
 
-  return display ? (
+  const readCookie = (name) => {
+    const match = document.cookie.match(new RegExp(`(^| )${name}=([^;]+)`));
+    if (match) {
+      return match[2];
+    }
+    return null;
+  };
+
+  const cookieConsent = readCookie('cookieConsent');
+
+  return !cookieConsent && display ? (
     <div id="cookie-banner">
       <p>This website uses cookies to ensure you get the best experience on our website.</p>
       <button type="submit" id="cookie-button" onClick={dismissBanner}>Got it!</button>
