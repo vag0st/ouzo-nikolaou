fd1be3cffa7448d127afbcd0bfc91c9b61344402
vag0st <zomgwtfwow@hotmail.com>
2023-02-15 03:26:55 +0200

    ADD Footer component and CSS WIP

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,14 +1,38 @@
-import React from 'react';
-// import './Footer.css';
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable jsx-a11y/label-has-associated-control */
+import React, { useState } from 'react';
+import { FiMail } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
+import './Footer.css';
 
 function Footer() {
+  const [email, setEmail] = useState('');
+
+  const onEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const onFormSubmit = (event) => {
+    event.preventDefault();
+    console.log('Email submitted: ', email);
+  };
+
   return (
-    <div>
-      <div>
-        <img src="/assets/images/logo.png" alt="Ouzo-Logo-Footer" />
-        <h1 className="header__title">Footer size 44</h1>
+    <footer>
+      <div className="footer-container">
+        <Link to="/" className="logo-container">
+          <img src="/assets/images/logo.png" alt="Ouzo Logo" className="header__logo" />
+        </Link>
+        <div className="email-form-container">
+          <FiMail className="mail-icon" />
+          <form onSubmit={onFormSubmit}>
+            <label htmlFor="email">Subscribe to our newsletter:</label>
+            <input type="email" id="email" value={email} onChange={onEmailChange} />
+            <button type="submit">Subscribe</button>
+          </form>
+        </div>
       </div>
-    </div>
+    </footer>
   );
 }
 
